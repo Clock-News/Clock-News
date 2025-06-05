d689113ade7091045401c710c6ec741c2d4533eb
Clock <hasnainarehman@protonmail.com>
2025-06-05 18:50:58 +0500

    Update home-responsive.js

--- home-responsive.js
+++ home-responsive.js
@@ -1,1 +1,9 @@
+<script>
+  const menuToggle = document.getElementById('menuToggle');
+  const navLinks = document.getElementById('navLinks');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+</script>
 
