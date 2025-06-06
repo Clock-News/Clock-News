564619f45eab092c8e209ea7e9c3ba723d1f7179
Clock <hasnainarehman@protonmail.com>
2025-06-06 08:22:44 +0500

    Update home-responsive.js

--- home-responsive.js
+++ home-responsive.js
@@ -1,9 +1,19 @@
-
-  document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.getElementById("menuToggle");
   const navLinks = document.getElementById("navLinks");
 
   menuToggle.addEventListener("click", function () {
     navLinks.classList.toggle("active");
+  });
+
+  // Toggle dropdowns in mobile menu
+  const dropdowns = navLinks.querySelectorAll(".dropdown > .dropbtn");
+
+  dropdowns.forEach(dropbtn => {
+    dropbtn.addEventListener("click", function (e) {
+      e.preventDefault(); // prevent link default
+      const dropdownParent = this.parentElement;
+      dropdownParent.classList.toggle("active");
+    });
   });
 });
